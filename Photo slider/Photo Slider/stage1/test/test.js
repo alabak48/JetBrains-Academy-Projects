8b2a8ba18b6664b909e2cab47497c45a022181b3
Anja Labak <anja.labak12345@gmail.com>
2022-11-08 10:37:26 +0100

    Update test.js

--- Photo slider/Photo Slider/stage1/test/test.js
+++ Photo slider/Photo Slider/stage1/test/test.js
@@ -7,14 +7,71 @@ class Test extends StageTest {
     page = this.getPage(pagePath)
 
     tests = [
+        // Test 1 - Check container '.slider-wrapper'
         this.node.execute(async() => {
             const wrapper = await this.page.findBySelector('.slider-wrapper');
             return wrapper ?
                 correct() :
                 wrong(`Your page must contain a slider-wrapper element.`)
         }),
+
+        // Test 2 - Check container '.slider'
+
+        this.node.execute(async() => {
+            const wrapper = await this.page.findBySelector('.slider');
+            return wrapper ?
+                correct() :
+                wrong(`Your page must contain a slider element.`)
+        }),
+
+        // Test 3 - Check h1 element
+        this.page.execute(() => {
+            this.mainTitle = document.getElementsByTagName('h1');
+            return this.mainTitle ?
+                correct() :
+                wrong(`Your page must contain main title`)
+        }),
+
+        // Test 4 - Check width and height of container 'slider'
+        this.node.execute(async () => {
+            let sliderWidth = await this.page.evaluate(async () => {
+                let slider = document.getElementById('slider');
+                return [slider.getBoundingClientRect().width, slider.getBoundingClientRect().height];
+            });
+
+            return sliderWidth[0] === 828 && Math.abs(sliderWidth[1] - 500) < 2 ?
+                correct() :
+                wrong(`Check size of slider container.`);
+        }),
+
+        // Test 5 - Check max-width of container 'slider'
+        this.node.execute(async () => {
+        const containerComputedStyles = await container.getComputedStyles()
+
+        if (containerComputedStyles.max-width: !== 'clamp(320px, 75vw, 800px)') {
+        return wrong('Wrong maximum width of the slider.')
+        }),
+
+       // Test 6 - Check border
+            this.page.execute(() => {
+                let border = window.getComputedStyle(this.summary).border;
+                return  border === '15px solid rgba(255, 255, 255, 0.234)' ?
+                    correct() :
+                    wrong(`Please, check border properties.`)
+            }),
+
+                // Test 7 - Check box-shadow
+                this.node.execute(async () => {
+                    const containerComputedStyles = await container.getComputedStyles()
+
+                    if (containerComputedStyles.box-shadow: !== '0 2px 15px rgba(0, 0, 0, 0.2), 0 2px 20px rgba(0, 0, 0, 0.25)') {
+                        return wrong('Please check box-shadow properties')
+                    }),
+
+
     ]
 
+}
 }
 
 it("Test stage", async () => {
