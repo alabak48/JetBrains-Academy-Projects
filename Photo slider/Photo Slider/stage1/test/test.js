55832e20b8c93caab9e42d16cee5f72656542e78
Anja Labak <anja.labak12345@gmail.com>
2022-11-08 09:43:06 +0100

    Initial Setup

---
+++ Photo slider/Photo Slider/stage1/test/test.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const pagePath = path.join(__dirname, '../src/index.html');
+const {StageTest, correct, wrong} = require('hs-test-web');
+
+class Test extends StageTest {
+
+    page = this.getPage(pagePath)
+
+    tests = [
+        this.node.execute(async() => {
+            const wrapper = await this.page.findBySelector('.slider-wrapper');
+            return wrapper ?
+                correct() :
+                wrong(`Your page must contain a slider-wrapper element.`)
+        }),
+    ]
+
+}
+
+it("Test stage", async () => {
+        await new Test().runTests()
+    }
+).timeout(30000);
