e78bb8cc926a236c9a94f3dd87472d1240f637d8
Anja Labak <anja.labak12345@gmail.com>
2022-11-23 11:51:52 +0100

    Update test.js

--- Photo slider/Photo Slider/stage1/test/test.js
+++ Photo slider/Photo Slider/stage1/test/test.js
@@ -32,10 +32,10 @@ class Test extends StageTest {
                 wrong(`Your page must contain main title`)
         }),
 
-        // Test 4 - Check width and height of container '.slider'
+        // Test 4 - Check width and height of container 'slider'
         this.node.execute(async () => {
             let slider = await this.page.evaluate(async () => {
-                let slider = document.getElementsByClassName('slider');
+                let slider = document.getElementsByClassName('slider')[0];
                 let neededSliderWidth = Math.round(window.innerWidth / 100 * 75);
                 let neededSliderHeight = Math.round(window.innerHeight / 100 * 75);
                 neededSliderWidth = neededSliderWidth < 320 ? 320 : (neededSliderWidth > 800 ? 800 : neededSliderWidth);
